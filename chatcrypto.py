598cc35be3f3e04da0dfcec37123bde9756eaa68
Zach Lefin <zachlefin@gmail.com>
2020-02-23 11:12:10 -0700

    more blockchain funcs;

--- chatcrypto.py
+++ chatcrypto.py
@@ -3,6 +3,7 @@ import json
 import datetime as dt
 import string
 import random
+from Crypto.Cipher import AES
 
 class Block(object):
 
@@ -19,11 +20,30 @@ class Block(object):
         return {'p': self._prev_hash, 't': self._dt, 'c': self._content}
 
     def compute_block_hash(self):
-        return hl.sha256(bytes(self._concat_all(), 'utf-8')).hexdigest()
+        return hl.sha256(str.encode(self._concat_all())).hexdigest()
 
     @staticmethod
-    def block_chain_to_json(bc: list):
-        return json.dumps([block.convert_dict() for block in bc])
+    def from_dict(d):
+        b = Block(str(d['p']), str(d['c']))
+        b._dt = str(d['t'])
+        return b
+
+
+def blockchain_to_json(bc: list):
+    return json.dumps([block.convert_dict() for block in bc])
+
+
+def encrypt_blockchain(bc: list, key: str, iv: str):
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+
+
+def encrypt_test(msg, key, iv):
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    cyphertext = encryptor.encrypt(msg)
+
+
+def rand_key():
+    return ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
 
 
 def rand_hash():
@@ -42,10 +62,13 @@ def build_bigboi(length, name):
     bcd = [block.convert_dict() for block in bc]
 
     with open(name, 'w') as f:
-        f.write(json.dumps(bcd))
+        f.write(json.dumps(bcd, indent=4))
+
 
 if __name__ == "__main__":
-    ti = dt.datetime.now()
-    build_bigboi(1000000, './data/bigboi2.json')
-    tf = dt.datetime.now()
-    print(tf - ti)
+    # ti = dt.datetime.now()
+    # build_bigboi(10, './data/bigboi1.json')
+    # tf = dt.datetime.now()
+    # print(tf - ti)
+    
+    print(encrypt_test('youre mum', 'ege123aaaaaaaaaa', 'wewladwewladwewl'))
